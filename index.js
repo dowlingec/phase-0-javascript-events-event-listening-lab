4ab98de5677ad834ab36d650cffd51fdaec5739b
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-24 10:04:07 -0400

    commit tree f996dba078e1972d6a7818d29f55aca189f1a128

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+
 function addingEventListener() {
+    input.addEventListener('click', function() {
+        alert('I done been clicked!')
+    })
 }
+addingEventListener()
