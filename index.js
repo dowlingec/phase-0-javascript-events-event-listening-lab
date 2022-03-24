6cabd0ecf794f28f8e0689f02a52dc12ada6e9dc
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-24 10:05:45 -0400

    passing

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    input.addEventListener('click', function() {
+        alert('I done been clicked!')
+    })
 }
+addingEventListener()
+
+// alt solution
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
